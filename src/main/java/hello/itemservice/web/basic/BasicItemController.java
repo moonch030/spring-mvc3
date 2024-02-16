f71216937a959edb73a96172c1b6169fdf3a3f0b
moonch030 <cogus2010@naver.com>
2024-02-16 16:21:21 +0900

    2024-02-16 RedirectAttribute

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -74,10 +75,17 @@ public class BasicItemController {
 //        model.addAttribute("item",item); //자동 추가, 생략 가능
         return "basic/item";
     }
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV5(Item item){ //첫글자가 소문자로 바뀜
         itemRepository.save(item);
         return "redirect:/basic/items/"+ item.getId();
+    }
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes){ //첫글자가 소문자로 바뀜
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
